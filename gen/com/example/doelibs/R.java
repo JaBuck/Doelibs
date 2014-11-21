1fb0b1302f8ee3e90441acb0481b743b4140c7b5
mattiasthiel <mattias@icreate.se>
2014-11-21 18:18:53 +0100

    changed misspelling

--- gen/com/example/doelibs/R.java
+++ gen/com/example/doelibs/R.java
@@ -35,10 +35,86 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int search_hint=0x7f050003;
+        public static final int button_borrow=0x7f050020;
+        public static final int button_cancel=0x7f050023;
+        public static final int button_confirmLoan=0x7f050028;
+        public static final int button_exit=0x7f05002e;
+        public static final int button_findLoan=0x7f05002b;
+        public static final int button_findRes=0x7f050026;
+        public static final int button_handout=0x7f050007;
+        public static final int button_login=0x7f050032;
+        public static final int button_logout=0x7f05000b;
+        public static final int button_main=0x7f05000c;
+        public static final int button_register=0x7f050033;
+        public static final int button_reserve=0x7f050018;
+        public static final int button_return=0x7f050008;
+        public static final int button_returnBook=0x7f05001d;
+        public static final int button_search=0x7f050004;
+        public static final int checkbox_push=0x7f05000a;
+        public static final int checkbox_remember=0x7f050031;
+        /**  details  
+         */
+        public static final int details=0x7f050015;
+        /**  error 
+         */
+        public static final int error=0x7f05002c;
+        /**  hand out 
+         */
+        public static final int handout=0x7f050024;
+        public static final int hello_world=0x7f050002;
+        public static final int hint_confirm=0x7f050038;
+        /**  login 
+         */
+        public static final int hint_email=0x7f05002f;
+        public static final int hint_firstname=0x7f050035;
+        public static final int hint_lastname=0x7f050036;
+        public static final int hint_password=0x7f050030;
+        /**  start page 
+         */
+        public static final int hint_search=0x7f050003;
+        public static final int hint_tagnumber=0x7f05002a;
+        public static final int label_borrowingBook=0x7f05001e;
+        public static final int label_borrowingExpired=0x7f05000e;
+        public static final int label_loanables=0x7f050016;
+        public static final int label_myBorrowings=0x7f050005;
+        public static final int label_myLoanables=0x7f050006;
+        public static final int label_pending=0x7f050027;
+        public static final int label_pendingReg=0x7f050012;
+        public static final int label_returningBook=0x7f05001b;
+        public static final int label_scanresult=0x7f05001a;
+        public static final int label_stop=0x7f050021;
+        public static final int label_titleAvaliable=0x7f050010;
+        public static final int lable_reservations=0x7f050017;
+        /**  notifications 
+         */
+        public static final int notifications=0x7f05000d;
+        /**  quick scan 
+         */
+        public static final int quickscan=0x7f050019;
+        /**  return book 
+         */
+        public static final int returnbook=0x7f050029;
+        /**  search result 
+         */
+        public static final int searchresult=0x7f050014;
+        /**  settings  
+         */
+        public static final int settings=0x7f050009;
+        /**  register 
+         */
+        public static final int spinner_category=0x7f050034;
+        public static final int spinner_handout=0x7f050025;
+        public static final int spinner_ref=0x7f050037;
+        public static final int text_borrowingBook=0x7f05001f;
+        public static final int text_borrowingExpired=0x7f05000f;
+        public static final int text_error=0x7f05002d;
+        public static final int text_pending=0x7f050039;
+        public static final int text_pendingReg=0x7f050013;
+        public static final int text_returningBook=0x7f05001c;
+        public static final int text_stop=0x7f050022;
+        public static final int text_titleAvaliable=0x7f050011;
     }
     public static final class style {
         /** 
