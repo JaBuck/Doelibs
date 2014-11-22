d6a2cf209c82542e7a0c1fb2c9aca6181ae68e35
mattiasthiel <mattias@icreate.se>
2014-11-22 10:53:15 +0100

    Early stage of fixing the action bar.

--- src/com/example/doelibs/MainActivity.java
+++ src/com/example/doelibs/MainActivity.java
@@ -18,19 +18,24 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.activity_main_actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+        
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_scan:
+                //openSearch();
+                return true;
+            case R.id.action_settings:
+               // openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
+
 }
