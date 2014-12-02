8b071bcc660f0886d09ffe0680aedcbe663f079b
mattiasthiel <mattias@icreate.se>
2014-12-02 12:19:56 +0100

    Changed application background color

--- gen/com/example/doelibs/R.java
+++ gen/com/example/doelibs/R.java
@@ -10,6 +10,9 @@ package com.example.doelibs;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bg=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,8 +21,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_action_scan=0x7f020000;
@@ -27,18 +30,18 @@ public final class R {
         public static final int x_ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int MyLoanablesList=0x7f080009;
-        public static final int Reservations=0x7f080004;
-        public static final int TitleAuthor=0x7f080001;
-        public static final int TitleEditonNr=0x7f080002;
-        public static final int TitleISBN=0x7f080003;
-        public static final int TitleId=0x7f080000;
-        public static final int action_scan=0x7f08000a;
-        public static final int action_settings=0x7f08000b;
-        public static final int buttonSearch=0x7f080008;
-        public static final int password=0x7f080006;
-        public static final int searchbox=0x7f080007;
-        public static final int username=0x7f080005;
+        public static final int MyLoanablesList=0x7f090009;
+        public static final int Reservations=0x7f090004;
+        public static final int TitleAuthor=0x7f090001;
+        public static final int TitleEditonNr=0x7f090002;
+        public static final int TitleISBN=0x7f090003;
+        public static final int TitleId=0x7f090000;
+        public static final int action_scan=0x7f09000a;
+        public static final int action_settings=0x7f09000b;
+        public static final int buttonSearch=0x7f090008;
+        public static final int password=0x7f090006;
+        public static final int searchbox=0x7f090007;
+        public static final int username=0x7f090005;
     }
     public static final class layout {
         public static final int activity_details=0x7f030000;
@@ -46,98 +49,98 @@ public final class R {
         public static final int activity_main=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main_actionbar=0x7f070000;
-        public static final int details=0x7f070001;
-        public static final int login=0x7f070002;
-        public static final int main=0x7f070003;
+        public static final int activity_main_actionbar=0x7f080000;
+        public static final int details=0x7f080001;
+        public static final int login=0x7f080002;
+        public static final int main=0x7f080003;
     }
     public static final class string {
         /**  toasts and errors 
  icons and actions 
          */
-        public static final int action_scan=0x7f05003a;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int button_borrow=0x7f050020;
-        public static final int button_cancel=0x7f050023;
-        public static final int button_confirmLoan=0x7f050028;
-        public static final int button_exit=0x7f05002e;
-        public static final int button_findLoan=0x7f05002b;
-        public static final int button_findRes=0x7f050026;
-        public static final int button_handout=0x7f050007;
-        public static final int button_login=0x7f050032;
-        public static final int button_logout=0x7f05000b;
-        public static final int button_main=0x7f05000c;
-        public static final int button_register=0x7f050033;
-        public static final int button_reserve=0x7f050018;
-        public static final int button_return=0x7f050008;
-        public static final int button_returnBook=0x7f05001d;
-        public static final int button_search=0x7f050004;
-        public static final int checkbox_push=0x7f05000a;
-        public static final int checkbox_remember=0x7f050031;
+        public static final int action_scan=0x7f06003a;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int button_borrow=0x7f060020;
+        public static final int button_cancel=0x7f060023;
+        public static final int button_confirmLoan=0x7f060028;
+        public static final int button_exit=0x7f06002e;
+        public static final int button_findLoan=0x7f06002b;
+        public static final int button_findRes=0x7f060026;
+        public static final int button_handout=0x7f060007;
+        public static final int button_login=0x7f060032;
+        public static final int button_logout=0x7f06000b;
+        public static final int button_main=0x7f06000c;
+        public static final int button_register=0x7f060033;
+        public static final int button_reserve=0x7f060018;
+        public static final int button_return=0x7f060008;
+        public static final int button_returnBook=0x7f06001d;
+        public static final int button_search=0x7f060004;
+        public static final int checkbox_push=0x7f06000a;
+        public static final int checkbox_remember=0x7f060031;
         /**  details 
          */
-        public static final int details=0x7f050015;
+        public static final int details=0x7f060015;
         /**  error 
          */
-        public static final int error=0x7f05002c;
+        public static final int error=0x7f06002c;
         /**  hand out 
          */
-        public static final int handout=0x7f050024;
-        public static final int hello_world=0x7f050002;
-        public static final int hint_confirm=0x7f050038;
+        public static final int handout=0x7f060024;
+        public static final int hello_world=0x7f060002;
+        public static final int hint_confirm=0x7f060038;
         /**  login 
          */
-        public static final int hint_email=0x7f05002f;
-        public static final int hint_firstname=0x7f050035;
-        public static final int hint_lastname=0x7f050036;
-        public static final int hint_password=0x7f050030;
+        public static final int hint_email=0x7f06002f;
+        public static final int hint_firstname=0x7f060035;
+        public static final int hint_lastname=0x7f060036;
+        public static final int hint_password=0x7f060030;
         /**  start page 
          */
-        public static final int hint_search=0x7f050003;
-        public static final int hint_tagnumber=0x7f05002a;
-        public static final int label_borrowingBook=0x7f05001e;
-        public static final int label_borrowingExpired=0x7f05000e;
-        public static final int label_loanables=0x7f050016;
-        public static final int label_myBorrowings=0x7f050005;
-        public static final int label_myLoanables=0x7f050006;
-        public static final int label_pending=0x7f050027;
-        public static final int label_pendingReg=0x7f050012;
-        public static final int label_returningBook=0x7f05001b;
-        public static final int label_scanresult=0x7f05001a;
-        public static final int label_stop=0x7f050021;
-        public static final int label_titleAvaliable=0x7f050010;
-        public static final int lable_reservations=0x7f050017;
+        public static final int hint_search=0x7f060003;
+        public static final int hint_tagnumber=0x7f06002a;
+        public static final int label_borrowingBook=0x7f06001e;
+        public static final int label_borrowingExpired=0x7f06000e;
+        public static final int label_loanables=0x7f060016;
+        public static final int label_myBorrowings=0x7f060005;
+        public static final int label_myLoanables=0x7f060006;
+        public static final int label_pending=0x7f060027;
+        public static final int label_pendingReg=0x7f060012;
+        public static final int label_returningBook=0x7f06001b;
+        public static final int label_scanresult=0x7f06001a;
+        public static final int label_stop=0x7f060021;
+        public static final int label_titleAvaliable=0x7f060010;
+        public static final int lable_reservations=0x7f060017;
         /**  notifications 
          */
-        public static final int notifications=0x7f05000d;
+        public static final int notifications=0x7f06000d;
         /**  quick scan 
          */
-        public static final int quickscan=0x7f050019;
+        public static final int quickscan=0x7f060019;
         /**  return book 
          */
-        public static final int returnbook=0x7f050029;
+        public static final int returnbook=0x7f060029;
         /**  search result 
          */
-        public static final int searchresult=0x7f050014;
+        public static final int searchresult=0x7f060014;
         /**  settings 
          */
-        public static final int settings=0x7f050009;
+        public static final int settings=0x7f060009;
         /**  register 
          */
-        public static final int spinner_category=0x7f050034;
-        public static final int spinner_handout=0x7f050025;
-        public static final int spinner_ref=0x7f050037;
-        public static final int text_borrowingBook=0x7f05001f;
-        public static final int text_borrowingExpired=0x7f05000f;
-        public static final int text_error=0x7f05002d;
-        public static final int text_pending=0x7f050039;
-        public static final int text_pendingReg=0x7f050013;
-        public static final int text_returningBook=0x7f05001c;
-        public static final int text_stop=0x7f050022;
-        public static final int text_titleAvaliable=0x7f050011;
-        public static final int title_activity_details=0x7f05003b;
-        public static final int title_activity_login=0x7f05003c;
+        public static final int spinner_category=0x7f060034;
+        public static final int spinner_handout=0x7f060025;
+        public static final int spinner_ref=0x7f060037;
+        public static final int text_borrowingBook=0x7f06001f;
+        public static final int text_borrowingExpired=0x7f06000f;
+        public static final int text_error=0x7f06002d;
+        public static final int text_pending=0x7f060039;
+        public static final int text_pendingReg=0x7f060013;
+        public static final int text_returningBook=0x7f06001c;
+        public static final int text_stop=0x7f060022;
+        public static final int text_titleAvaliable=0x7f060011;
+        public static final int title_activity_details=0x7f06003b;
+        public static final int title_activity_login=0x7f06003c;
     }
     public static final class style {
         /** 
@@ -161,10 +164,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int Doelibs=0x7f070001;
     }
 }
